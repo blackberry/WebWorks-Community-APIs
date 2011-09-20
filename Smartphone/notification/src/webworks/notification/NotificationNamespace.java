8e9109ed27949b01a32a29f550c6585f6f87cc1c
John Mutter <jmutter@rim.com>
2011-09-20 09:10:48 -0400

    Updated java files to fix some bugs

--- Smartphone/notification/src/webworks/notification/NotificationNamespace.java
+++ Smartphone/notification/src/webworks/notification/NotificationNamespace.java
@@ -16,15 +16,13 @@
 
 package webworks.notification;
 
-import net.rim.blackberry.api.messagelist.ApplicationIndicator;
-import net.rim.blackberry.api.messagelist.ApplicationIndicatorRegistry;
 import net.rim.device.api.notification.NotificationsConstants;
 import net.rim.device.api.notification.NotificationsManager;
 import net.rim.device.api.script.Scriptable;
 import net.rim.device.api.script.ScriptableFunction;
 import net.rim.device.api.util.StringUtilities;
 
-public class NotificationNamespace  extends Scriptable
+public class NotificationNamespace extends Scriptable
 {
     public static final String FIELD_REGISTER = "register";
     public static final String FIELD_FIRE = "fire";
@@ -35,7 +33,7 @@ public class NotificationNamespace  extends Scriptable
     {
         if (name.equals(FIELD_REGISTER))
         {
-            return new RegisterNotificationObjects();
+            return new RegisterNotification();
         }
         else if (name.equals(FIELD_FIRE))
         {
@@ -47,71 +45,99 @@ public class NotificationNamespace  extends Scriptable
         }
         else if (name.equals(FIELD_UNREGISTER))
         {
-            return new UnregisterNotificationObjects();
+            return new UnregisterNotification();
         }
         return super.getField(name);
     }
 
-    private class RegisterNotificationObjects  extends ScriptableFunction
+    public class RegisterNotification extends ScriptableFunction
     {
         private static final String NAME = "register";
-        private Object invoke(Object obj, Object[] args) throws Exception
+        public Object invoke(Object obj, Object[] args) throws Exception
         {
             if (args != null && args.length == 2)
             {
-                final long sourceID = StringUtilities.stringHashToLong(args[0].toString());
-                final String notificationName =  args[1].toString();
-                NotificationsManager.registerSource( 
-                    sourceID, new Object()
-                    {
-                        public String toString()
+                try
+                {
+                    final long sourceID = StringUtilities.stringHashToLong(args[0].toString());
+                    final String notificationName =  args[1].toString();
+                    NotificationsManager.registerSource( 
+                        sourceID, new Object()
                         {
-                            return notificationName;
+                            public String toString()
+                            {
+                                return notificationName;
+                            }
                         }
-                    }
-                    , NotificationsConstants.IMPORTANT );
+                        , NotificationsConstants.IMPORTANT );
+                }
+                catch(final Exception e)
+                {
+                    throw new RuntimeException(e.getMessage());
+                }
             }
             return UNDEFINED;
         }
     }
 
-    private class FireEvent  extends ScriptableFunction
+    public class FireEvent extends ScriptableFunction
     {
         private static final String NAME = "fire";
-        private Object invoke(Object obj, Object[] args) throws Exception
+        public Object invoke(Object obj, Object[] args) throws Exception
         {
             if (args != null && args.length == 1)
             {
-                final long sourceID = StringUtilities.stringHashToLong(args[0].toString());
-                NotificationsManager.triggerImmediateEvent( sourceID, 0, this, null );
+                try
+                {
+                    final long sourceID = StringUtilities.stringHashToLong(args[0].toString());
+                    NotificationsManager.triggerImmediateEvent( sourceID, 0, this, null );
+                }
+                catch(final Exception e)
+                {
+                    throw new RuntimeException(e.getMessage());
+                }
             }
             return UNDEFINED;
         }
     }
 
-    private class CancelEvent  extends ScriptableFunction
+    public class CancelEvent extends ScriptableFunction
     {
         private static final String NAME = "cancel";
-        private Object invoke(Object obj, Object[] args) throws Exception
+        public Object invoke(Object obj, Object[] args) throws Exception
         {
             if (args != null && args.length == 1)
             {
-                final long sourceID = StringUtilities.stringHashToLong(args[0].toString());
-                NotificationsManager.cancelImmediateEvent( sourceID, 0, this, null );
+                try
+                {
+                    final long sourceID = StringUtilities.stringHashToLong(args[0].toString());
+                    NotificationsManager.cancelImmediateEvent( sourceID, 0, this, null );
+                }
+                catch(final Exception e)
+                {
+                    throw new RuntimeException(e.getMessage());
+                }
             }
             return UNDEFINED;
         }
     }
 
-    private class UnregisterNotificationObjects  extends ScriptableFunction
+    public class UnregisterNotification extends ScriptableFunction
     {
         private static final String NAME = "unregister";
-        private Object invoke(Object obj, Object[] args) throws Exception
+        public Object invoke(Object obj, Object[] args) throws Exception
         {
             if (args != null && args.length == 1)
             {
-                final long sourceID = StringUtilities.stringHashToLong(args[0].toString());
-                NotificationsManager.deregisterSource(sourceID);
+                try
+                {
+                    final long sourceID = StringUtilities.stringHashToLong(args[0].toString());
+                    NotificationsManager.deregisterSource(sourceID);
+                }
+                catch(final Exception e)
+                {
+                    throw new RuntimeException(e.getMessage());
+                }
             }
             return UNDEFINED;
         }
