dbb584a2eb33de4d372019ac1f22fa3d8ad944c0
Brent Thornton <bthornton@rim.com>
2012-03-15 15:39:45 -0400

    Added Apache license and removed commented code

--- Smartphone/LocationPicker/blackberry/ui/locationPicker/LocationPickerFunction.java
+++ Smartphone/LocationPicker/blackberry/ui/locationPicker/LocationPickerFunction.java
@@ -1,3 +1,19 @@
+
+/*
+* Copyright 2010-2011 Research In Motion Limited.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
 package blackberry.ui.locationPicker;
 
 import javax.microedition.location.Landmark;
@@ -16,14 +32,10 @@ public final class LocationPickerFunction extends ScriptableFunction {
 	
 	public Object invoke(Object obj, Object[] args) throws Exception
 	{
-		/*if (!mapsExist()) {
-			return UNDEFINED;
-		}*/
 		if (args.length == 1) {
 			// Now get the callback method to fire
 			final ScriptableFunction _callback = (ScriptableFunction)args[0];
 
-			//final UiApplication uiApp = UiApplication.getUiApplication();	
 			UiApplication.getUiApplication().invokeLater(new Runnable()
 			{
 				public void run()
@@ -38,7 +50,6 @@ public final class LocationPickerFunction extends ScriptableFunction {
 			//if they pass the optional parameter. The default location
 			final Scriptable ops = (Scriptable) args[1];
 			
-			//final UiApplication uiApp = UiApplication.getUiApplication();	
 			UiApplication.getUiApplication().invokeLater(new Runnable()
 			{
 				public void run()
