1bf0c6d809e010d224b3ff60fa27977fc873ea73
Rob Williams <robwilliams@rim.com>
2013-03-11 11:24:05 -0400

    Updated to actually do base64

--- BB10/gseCrypto/NDK_project/src/util/util.cpp
+++ BB10/gseCrypto/NDK_project/src/util/util.cpp
@@ -4,7 +4,7 @@ namespace gsecrypto {
 namespace util {
 
 std::string toB64(unsigned char * data, size_t dataLen) {
-	const char * b64Chars =
+	static const char * b64Chars =
 			"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
 	std::string toReturn;
 	for (size_t i = 0; i < dataLen; i += 3) {
@@ -13,7 +13,7 @@ std::string toB64(unsigned char * data, size_t dataLen) {
 
 		unsigned char a = data[i];
 		unsigned char b = bgood ? data[i + 1] : 0;
-		unsigned char c = cgood ? data[i + 1] : 0;
+		unsigned char c = cgood ? data[i + 2] : 0;
 
 		toReturn += b64Chars[a >> 2];
 		toReturn += b64Chars[((a & 3) << 4) | b >> 4];
@@ -46,10 +46,8 @@ void fromHex(std::string en, unsigned char * & data, size_t & dataLen) {
 	dataLen = encoded.length() / 2;
 	data = new unsigned char[dataLen];
 
-	int offset = 0;
 	for (size_t i = 0; i < dataLen; ++i) {
-		data[i] = nibble(encoded[offset++]);
-		data[i] = nibble(encoded[offset++]);
+		data[i] = (nibble(encoded[i<<1]) << 4) | nibble(encoded[(i<<1)+1]);
 	}
 }
 
@@ -60,6 +58,22 @@ int nibble(const char c) {
 		return c - '0';
 	}
 }
+
+unsigned char b64Nibble(unsigned char c) {
+	if (c>='A' && c<='Z') {
+		return c-'A';
+	} else if (c>='a' && c<='z') {
+		return c-'a'+26;
+	} else if (c>='0' && c<='9') {
+		return c-'0'+52;
+	} else if (c=='+') {
+		return 62;
+	} else if (c=='/') {
+		return 63;
+	}
+	return 0;
+}
+
 
 void fromB64(std::string encoded, unsigned char * & data, size_t & dataLen) {
 	std::string encoded2;
@@ -99,11 +113,11 @@ void fromB64(std::string encoded, unsigned char * & data, size_t & dataLen) {
 		unsigned char v[3];
 		unsigned char e[4];
 		for (int j = 0; j < 4; ++j) {
-			e[j] = encoded2[offset++];
+			e[j] = b64Nibble(encoded2[offset++]);
 		}
 		v[0] = e[0] << 2 | ((e[1] >> 4) & 0x3);
 		v[1] = e[1] << 4 | ((e[2] >> 2) & 0xf);
-		v[2] = e[2] << 6 | ((e[3] & 0x2f));
+		v[2] = e[2] << 6 | ((e[3] & 0x3f));
 		for (int j = 0; j < 3 && outOffset < dataLen; ++j) {
 			data[outOffset++] = v[j];
 		}
