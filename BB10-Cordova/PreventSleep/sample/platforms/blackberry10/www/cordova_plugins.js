6048fbd964bde806317f0e91e670a1dbd12824d1
timwindsor <timothy.windsor@gmail.com>
2013-08-16 17:08:39 -0400

    Updated working sample

---
+++ BB10-Cordova/PreventSleep/sample/platforms/blackberry10/www/cordova_plugins.js
@@ -0,0 +1,11 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins\\community.preventsleep\\www\\client.js",
+        "id": "community.preventsleep.client",
+        "clobbers": [
+            "community.preventsleep"
+        ]
+    }
+]
+});
