cdfcf57d62101ca5ede1e6b6c67571e2e73e9611
Daniel <danieru.dressler@gmail.com>
2013-09-21 11:42:57 -0600

    Add ExtractZIPFile plugin shell

---
+++ BB10-Cordova/ExtractZIPFile/sample/platforms/blackberry10/www/cordova_plugins.js
@@ -0,0 +1,11 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/community.extractzipfile/www/client.js",
+        "id": "community.extractzipfile.client",
+        "clobbers": [
+            "community.extractzipfile"
+        ]
+    }
+]
+});
