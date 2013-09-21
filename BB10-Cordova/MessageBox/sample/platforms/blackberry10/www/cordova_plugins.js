d4e67c7b3ddf016c6e0a5a1e42da8df668dae1c0
Jasmin Auger <jasmin@leflasheux.com>
2013-09-21 16:46:57 -0400

    Use corodova plugin MessageBox.Apply and MessageBox.Confirm.

--- BB10-Cordova/MessageBox/sample/platforms/blackberry10/www/cordova_plugins.js
+++ BB10-Cordova/MessageBox/sample/platforms/blackberry10/www/cordova_plugins.js
@@ -1,6 +1,20 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
 module.exports = [
     {
+        "file": "plugins/org.apache.cordova.core.dialogs/www/notification.js",
+        "id": "org.apache.cordova.core.dialogs.notification",
+        "merges": [
+            "navigator.notification"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.core.dialogs/www/blackberry10/beep.js",
+        "id": "org.apache.cordova.core.dialogs.beep",
+        "clobbers": [
+            "window.navigator.notification.beep"
+        ]
+    },
+    {
         "file": "plugins/org.apache.cordova.MessageBox/www/client.js",
         "id": "org.apache.cordova.MessageBox.client",
         "clobbers": [
--- BB10-Cordova/MessageBox/sample/platforms/blackberry10/www/plugins/org.apache.cordova.MessageBox/www/client.js
+++ BB10-Cordova/MessageBox/sample/platforms/blackberry10/www/plugins/org.apache.cordova.MessageBox/www/client.js
@@ -11,52 +11,49 @@ cordova.define("org.apache.cordova.MessageBox.client", function(require, exports
         cancelButtonTitle: 'Cancel'
     };
 
+    // MessageBox.prototype.alert = function(options, callback) {
+    //     options || (options = {})
+    //     var D_OK = 0;
+    //     var message = options.message || '';
+    //     var settings = {'title' : options.title || ''};
+
+    //     var args = { "message" : message, "type" : D_OK, "callback" : callback };
+    //     if (settings) {
+    //         args.settings = settings;
+    //     }
+    //     exec(callback, function () {}, _ID, "standardAskAsync", args);
+    // };
+
+    // MessageBox.prototype.confirm = function(options, callback) {
+    //     options || (options = {})
+    //     var message = options.message || '';
+    //     var settings = {'title' : options.title || ''};
+    //     var btns = [options.yesButtonTitle || 'Yes',
+    //                 options.noButtonTitle || 'No'];
+    //     var args = { "message" : message, "callback" : callback, buttons: btns };
+        
+    //     if (settings) {
+    //         args.settings = settings;
+    //     }
+    //     exec(callback, function () {}, _ID, "customAskAsync", args);
+    // };
+
     MessageBox.prototype.alert = function(options, callback) {
+        options || (options = {});
+        var scope = options.scope || null;
 
-        var D_OK = 0;
-        var message = options['message'] || '';
-        var settings = {'title' : options['title'] || ''};
+        var config = {
+            title: options.title || '',
+            message: options.message || '',
+            okButtonTitle: options.okButtonTitle || this.defaults.okButtonTitle
+        };
 
-        var args = { "message" : message, "type" : D_OK, "callback" : callback };
-        if (settings) {
-            args.settings = settings;
-        }
-        exec(callback, function () {}, _ID, "standardAskAsync", args);
+        var _callback = function(buttonIndex) {
+            var button = 'ok';
+            if(typeof callback == 'function') callback.call(scope, button);
+        };
 
-        // var D_OK = 0;
-        // var message = options['message'] || '';
-        // var settings = {'title' : options['title'] || ''};
-
-        // var result,
-        //     success = function (data, response) {
-        //         result = data;
-        //     },
-        //     fail = function (data, response) {
-        //         console.log("Error: " + data);
-        //     };
-
-
-        // exec(success, fail, _ID, "testInput", { message, D_OK, callback, settings });
-
-        // return result;
-
-        // blackberry.ui.dialog.standardAskAsync(message, D_OK, callback, settings);
-
-        // options || (options = {});
-        // var scope = options.scope || null;
-
-        // var config = {
-        //     title: options.title || '',
-        //     message: options.message || '',
-        //     okButtonTitle: options.okButtonTitle || this.defaults.okButtonTitle
-        // };
-
-        // var _callback = function(buttonIndex) {
-        //     var button = 'ok';
-        //     if(typeof callback == 'function') callback.call(scope, button);
-        // };
-
-        // return navigator.notification.alert(config.message, _callback, config.title, config.okButtonTitle + '');
+        return navigator.notification.alert(config.message, _callback, config.title, config.okButtonTitle + '');
     };
 
     MessageBox.prototype.confirm = function(options, callback) {
@@ -77,7 +74,7 @@ cordova.define("org.apache.cordova.MessageBox.client", function(require, exports
 
         return navigator.notification.confirm(config.message, _callback, config.title, config.noButtonTitle + ', ' + config.yesButtonTitle);
     };
-
+    
     MessageBox.prototype.prompt = function(options, callback) {
         options || (options = {});
         var scope = options.scope || null;
@@ -102,7 +99,6 @@ cordova.define("org.apache.cordova.MessageBox.client", function(require, exports
 
     cordova.addConstructor(function() {
         if(!window.plugins) window.plugins = {};
-        console.log("ADDCONS")
         window.plugins.messageBox = new MessageBox();
     });
 
