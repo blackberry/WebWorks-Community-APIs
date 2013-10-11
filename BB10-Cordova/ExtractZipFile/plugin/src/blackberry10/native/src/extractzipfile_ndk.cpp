41e8415f8f7743c87733dd84b09447feaac7e590
danieru <danieru.dressler@gmail.com>
2013-10-10 23:36:27 -0600

    Add interation and extraction over zip contents

--- BB10-Cordova/ExtractZipFile/plugin/src/blackberry10/native/src/extractzipfile_ndk.cpp
+++ BB10-Cordova/ExtractZipFile/plugin/src/blackberry10/native/src/extractzipfile_ndk.cpp
@@ -45,6 +45,11 @@ void ExtractZIPFileNDK::extractFile(const std::string& callbackId, const std::st
 		retval["result_message"] = y; \
 		m_pParent->NotifyEvent(callbackId + " " + writer.write(retval));}
 
+	// Tune this and reduce mem usage
+	#define EZIPBUFSIZE 1024
+	#define MAX_FILENAME 1024
+
+
 	// Parse the arg string as JSON
 	Json::FastWriter writer;
 	Json::Reader reader;
@@ -62,11 +67,106 @@ void ExtractZIPFileNDK::extractFile(const std::string& callbackId, const std::st
 	if (zipFile == NULL)
 		extractReturn(-1, "Failed to open zip file.");
 
+	// get zip metadata
+	unz_global_info zipInfo;
+	if (unzGetGlobalInfo(zipFile, &zipInfo) != UNZ_OK) {
+		unzClose(zipFile);
+		extractReturn(-1, "Failed to parse zip metadata.");
+	}
+
+	// fixed size buf on stack, WATCH USAGE!
+	char fileBuffer[EZIPBUFSIZE];
+
+	int filesExtract = 0;
+	for (int i = 0; i < zipInfo.number_entry; i++) {
+
+		// single file metadata
+		unz_file_info fileInfo;
+		char filename[MAX_FILENAME + 1];
+
+		// get metadata on specific file
+		if (unzGetCurrentFileInfo(
+					zipFile,
+					&fileInfo,
+					filename,
+					MAX_FILENAME,
+					NULL, 0, NULL, 0) != UNZ_OK) {
+			unzClose(zipFile);
+			extractReturn(-1, "Failed to parse a file's metadata.");
+		}
+		filename[MAX_FILENAME] = "\0"; // ensure string termination
+
+		// TODO: Support removing created dirs and files upon any failure
+		//       keep vector of filenames
+
+		// Handle Directories
+		if (filename[strlen(filename) - 1] == dir_delimiter) {
+			// Directory creation cannot lose data
+			// so we do not care if a dir already
+			// exists
+			mkdir(filename);
+
+		// Handle Files
+		} else {
+			// TODO: check file existance and respect passed in options
+
+			// Note: This opens zipFile's "current" file
+			//       "current" acts as an interator
+			if (unzOpenCurrentFile(zipFile) != UNZ_OK) {
+				unzClose(zipFile);
+				extractReturn(-1, "Failed to extract file: " + filename);
+			}
+
+			// Open destination file in file system
+			FILE *destFile = fopen(filename, "wb");
+			if (destFile == NULL) {
+				unzCloseCurrentFile(zipFile);
+				unzClose(zipFile);
+				extractReturn(-1, "Failed to open destination file: " + filename);
+			}
+
+			// Ferry data into destination
+			int readResult = UNZ_OK; // is 0
+			do {
+				// Read
+				readResult = unzReadCurrentFile(zipFile, fileBuffer, EZIPBUFSIZE);
+				if (readResult < 0) {
+					unzCloseCurrentFile(zipFile);
+					unzClose(zipFile);
+					fclose(destFile);
+					extractReturn(-1, "Failed to read compressed file: " + filename);
+				}
+
+				// Write
+				if (readResult > 0) {
+					int writeResult = fwrite(fileBuffer, readResult, 1, destFile);
+					if (writeResult != 1) {
+						// Note: we asked for the full buffer
+						// to be written at once, so the 1
+						// return value is not "true" but
+						// the number blocks writen
+						unzCloseCurrentFile(zipFile);
+						unzClose(zipFile);
+						fclose(destFile);
+						extractReturn(-1, "Failed to write to destination file: " + filename);
+					}
+				}
+			} while (readResult > 0);
+
+			fclose(destFile);
+			filesExtracted++;
+		}
+
+		unzCloseCurrentFile(zipFile);
+
+		// Increment "current" file interator if any more files
+		unzGoToNextFile(zipFile);
+	}
+
 	unzClose(zipFile);
 
 	// Success!
-	extractReturn(0, "Extract successful.");
-	#undef extractReturn
+	extractReturn(filesExtracted, "Extraction successful.");
 }
 
 } /* namespace webworks */
