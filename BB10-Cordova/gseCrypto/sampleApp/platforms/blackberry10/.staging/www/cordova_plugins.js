d160e41ef372e135a7acec5ae0472f3cd9aebbb0
Jim Wood <jpwood@uwaterloo.ca>
2014-02-07 16:25:12 -0500

    copied Template to gseCrypto, created sampleApp - basic baseline start point

---
+++ BB10-Cordova/gseCrypto/sampleApp/platforms/blackberry10/.staging/www/cordova_plugins.js
@@ -0,0 +1,17 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/community.templateplugin/www/client.js",
+        "id": "community.templateplugin.client",
+        "clobbers": [
+            "community.templateplugin"
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "community.templateplugin": "1.0.0"
+}
+// BOTTOM OF METADATA
+});
