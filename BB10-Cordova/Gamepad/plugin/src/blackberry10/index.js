8e26c782ccfb1e861b47610cdc36a769d3716237
timwindsor <timothy.windsor@gmail.com>
2014-06-02 16:57:21 -0400

    Adds the JS portion of the Gamepad plugin

---
+++ BB10-Cordova/Gamepad/plugin/src/blackberry10/index.js
@@ -0,0 +1,106 @@
+/*
+* Copyright (c) 2014 BlackBerry Limited
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+var joypad,
+	resultObjs = {},
+	threadCallback = null,
+   _utils = require("../../lib/utils");
+
+module.exports = {
+
+	start: function (success, fail, args) {
+		var result = new PluginResult(args, env);
+		resultObjs[result.callbackId] = result;
+		threadCallback = result.callbackId;
+		success(joypad.getInstance().start(result.callbackId));
+	},
+	stop: function (success, fail, args) {
+		var result = new PluginResult(args, env);
+		resultObjs[result.callbackId] = result;
+		success(joypad.getInstance().stop());
+	}
+};
+
+///////////////////////////////////////////////////////////////////
+// JavaScript wrapper for JNEXT plugin for connection
+///////////////////////////////////////////////////////////////////
+
+JNEXT.JoyPad = function () {
+	var self = this,
+		hasInstance = false;
+
+	self.getId = function () {
+		return self.m_id;
+	};
+
+	self.init = function () {
+		if (!JNEXT.require("libGamepad")) {
+			return false;
+		}
+
+		self.m_id = JNEXT.createObject("libGamepad.joypadJS");
+
+		if (self.m_id === "") {
+			return false;
+		}
+
+		JNEXT.registerEvents(self);
+	};
+
+	// ************************
+	// Enter your methods here
+	// ************************
+
+	// calls into InvokeMethod(string command) in joypad_js.cpp
+	self.start = function (callbackId) {
+		return JNEXT.invoke(self.m_id, "start " + callbackId);
+	};
+	self.stop = function () {
+		return JNEXT.invoke(self.m_id, "stop");
+	};
+	// Fired by the Event framework (used by asynchronous callbacks)
+	self.onEvent = function (strData) {
+		var arData = strData.split(" "),
+			callbackId = arData[0],
+			result = resultObjs[callbackId],
+			data = arData.slice(1, arData.length).join(" ");
+		
+		if (result) {
+			if (callbackId != threadCallback) {
+				result.callbackOk(JSON.parse(data), false);
+				delete resultObjs[callbackId];
+			} else {
+				result.callbackOk(JSON.parse(data), true);
+			}
+		}
+	};
+
+	// ************************
+	// End of methods to edit
+	// ************************
+	self.m_id = "";
+
+	self.getInstance = function () {
+		if (!hasInstance) {
+			hasInstance = true;
+			self.init();
+		}
+		return self;
+	};
+
+};
+
+joypad = new JNEXT.JoyPad();
